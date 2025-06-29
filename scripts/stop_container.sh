385c7bc190483a51a537071825d111b7abf342e9
tj330 <titusjamesranny@gmail.com>
2025-06-30 00:16:42 +0530

    edit stop script

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -10,9 +10,3 @@ if [ -n "$container_ids" ]; then
 else
   echo "No running containers found."
 fi
-
-# Build the Docker image
-docker build -t simple-python-flask-app .
-
-# Run the new Docker container
-docker run -d -p 80:80 simple-python-flask-app
